309dbba1bc4d4420fdb722d99c956435bef36d68
Pasi Manninen <pasi@ptm.fi>
2015-10-26 16:34:54 +0200

    Initial commit

---
+++ app/src/main/java/fi/ptm/viewpagerexample/AboutFragment.java
@@ -0,0 +1,23 @@
+package fi.ptm.viewpagerexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by PTM on 26/10/15.
+ */
+public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("About Fragment Content");
+        return rootView;
+    }
+
+}
